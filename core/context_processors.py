1ae67926cb8a33a7aa40c607460d61902db0d4b7
blakebinford <binford.blake@gmail.com>
2025-07-31 10:28:27 -0500

    meta update

--- core/context_processors.py
+++ core/context_processors.py
@@ -4,9 +4,9 @@ def default_meta(request):
     return {
         'meta': Meta(
             title="Atlas Competition",
-            description="Built for the future of strength sports.",
+            description="Atlas Competition is the all-in-one platform for managing and competing in strongman events. Discover upcoming competitions, register online, track scores live, and showcase athlete profiles. All in one place.",
             url=request.build_absolute_uri(),
-            image="https://atlascompetition.com/static/images/logo.png",
+            image="https://atlascompetition.com/static/images/navabarlogo.png",
             object_type='website',
             site_name='Atlas Competition',
         )
